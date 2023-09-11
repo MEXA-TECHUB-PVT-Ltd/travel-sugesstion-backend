51c974aa3d6fc5b82786adc33cbfd47c2f4d0408
MuhammadMTechHub <144309231+MuhammadMTechHub@users.noreply.github.com>
2023-09-11 10:17:42 +0500

    Revert "TS-221 TravelSuggestion First Commit"

--- routes/rating.routes.js
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import { createRating, getAllRatingsByUser, getRatingByPlace } from '../Controller/ratingController.js';
-
-const ratingRoute = Router();
-
-ratingRoute.post('/create', createRating);
-ratingRoute.get('/getRatingsByPlace/:place_id', getRatingByPlace);
-ratingRoute.get('/getAllRatingsByUser/:user_id', getAllRatingsByUser);
-export default ratingRoute;
